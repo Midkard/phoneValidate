6888a1fe8218c1b49f23bb9be52099afe5eeb313
Midkard <dimigam@yandex.ru>
2020-08-18 14:01:38 +0300

    1.0.1

--- src/phone.js
+++ src/phone.js
@@ -1,31 +1,31 @@
 'use strict';
-(function ( $ ) {
+(function ($) {
     $.fn.phoneMask = function () {
         // Если коллекция пуста, то заканчиваем сразу
-        if ( !this.length ) {
+        if (!this.length) {
             return;
         }
 
         //Здесь проходим по каждому элементу коллекции
-        this.each( function () {
+        this.each(function () {
             // Проверяем не создавали ли валидатор для этого элемента. Если да, то выходим.
-            var phoneMask = $( this ).data( "phoneMask" );
-            if ( phoneMask ) {
+            var phoneMask = $(this).data("phoneMask");
+            if (phoneMask) {
                 return;
             }
 
-            phoneMask = new PhoneField( this );
-            $( this ).data( "phoneMask", phoneMask );
-        } );
+            phoneMask = new PhoneField(this);
+            $(this).data("phoneMask", phoneMask);
+        });
 
         return this;
     };
 
-    function PhoneField( elem ) {
+    function PhoneField(elem) {
         this.elem = $(elem);
-        
+
         //Привязываем обработчики к собятиям элемента
-        $( elem ).on( 'input', this._transformToPhone.bind(this) );
+        $(elem).on('input', this._transformToPhone.bind(this));
 
     }
 
@@ -33,38 +33,38 @@
     PhoneField.prototype._transformToPhone = function () {
 
         var value = this.elem.val();
-        if ( !value ) {
+        if (!value) {
             return;
         }
 
-        value = value.replace( /\D/g, '' );
-        if ( !value.length ) {
-            this.elem.val( '' );
+        value = value.replace(/\D/g, '');
+        if (!value.length) {
+            this.elem.val('');
             return;
         }
-        if ( '7' === value[0] || '8' === value[0] ) {
-            value = value.slice( 1 );
+        if ('7' === value[0] || '8' === value[0]) {
+            value = value.slice(1);
         }
 
         var newValue = '+7(';
-        var groups = [value.slice( 0, 3 ), value.slice( 3, 6 ), value.slice( 6, 8 ), value.slice( 8, 10 )];
+        var groups = [value.slice(0, 3), value.slice(3, 6), value.slice(6, 8), value.slice(8, 10)];
 
-        groups = groups.filter( function ( el ) {
+        groups = groups.filter(function (el) {
             return el !== '';
-        } );
+        });
 
-        groups.forEach( function ( el, index ) {
-            if ( index === 1 ) {
+        groups.forEach(function (el, index) {
+            if (index === 1) {
                 newValue += ') ';
             }
-            if ( index > 1 ) {
+            if (index > 1) {
                 newValue += '-';
             }
             newValue += el;
-        } );
+        });
 
-        this.elem.val( newValue );
+        this.elem.val(newValue);
 
     };
 
-})( jQuery );
+})(jQuery);
